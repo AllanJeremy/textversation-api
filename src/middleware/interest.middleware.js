5925e61bc9c886a4508969161ae438bc599bb642
AllanJeremy <aj.dev254@gmail.com>
2019-10-20 13:31:29 +0300

    [Feature] Get all interests working

--- src/middleware/interest.middleware.js
+++ src/middleware/interest.middleware.js
@@ -1,17 +1,30 @@
 const Api = require("../lib/api");
 const Interest = require("../modules/interest");
 
+let _getInterestData = (interestDocs)=>{
+    return interestDocs.map((doc)=>{
+        let docData = {};
+        docData[doc.id] =  doc.data();
+        return docData;
+    });
+};
+
 module.exports.getAllInterests = (req,res,next)=>{
-    let interests = Interest.getAllInterests().then(resolve);
-    
-    if(interests){
-        let responseData = Api.getResponse(true,"Successfully retrieved all interests",interests);
-        res.status(200).json(responseData);
+    Interest.getAllInterests().then((response)=>{ // response : QuerySnaspshot
+        let isOk = !response.empty;
+        let responseStatus =  isOk ? 200 : 404;
+        let message = isOk ? "Successfully retrieved all interests" : "Interests not found";
+        let data = _getInterestData(response.docs);
+
+        let responseData = Api.getResponse(isOk,message,data,responseStatus);
+        
+        res.status(responseData.statusCode).json(responseData);
         next();
-    }else{ // Failed
+    }).catch((err)=>{
         console.error("Something went wrong while trying to get all interests");
         console.error(err.message);
-        let responseData = Api.getError("err.message,err");
+        
+        let responseData = Api.getError(err.message,err);
         res.status(500).json(responseData);
-    }
+    });
 };
