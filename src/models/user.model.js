bb4fc3f4895fae5c537688b3e832d765978fcbf8
AllanJeremy <aj.dev254@gmail.com>
2019-10-20 12:56:07 +0300

    Changed static methods to non-static methods in UserModel - Exporting instance of the class

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,15 +2,15 @@ const {db} = require("../database");
 
 class UserModel{
     // Update user ~ creates user if the user does not exist
-    static async updateUser(uid,updateData){
+    async updateUser(uid,updateData){
         return db.collection("users").doc(uid).set(updateData,{merge:true});
     }
     
     // Delete user
-    static async deleteUser(uid){
+    async deleteUser(uid){
         return db.collection("users").doc(uid).delete();
     }
 
 }
 
-module.exports = UserModel;
+module.exports = new UserModel();
