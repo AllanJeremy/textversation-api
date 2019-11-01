e06615eb2777a1e2001e4dc12ea5487c203f7439
AllanJeremy <aj.dev254@gmail.com>
2019-11-01 14:45:58 +0300

    [Enhancement] Api lib now returns the function after the error handling incase we want to do anything with it

--- src/lib/api.js
+++ src/lib/api.js
@@ -16,7 +16,7 @@ module.exports.getError = (message, error, statusCode) => {
 
 // Attaches an error handler to a function ~ returns an appropriate response
 module.exports.attachErrorHandler = (res,fn)=>{ //? Intended to be called in middleware
-    fn.catch(err=>{
+    return fn.catch(err=>{
         let apiResponse = this.getError(err.message,err);
         console.log(err);
         console.error(err.message);
