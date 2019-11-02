76ce31256a8d47891daf4dc3d1d78fc7dd948d9f
AllanJeremy <aj.dev254@gmail.com>
2019-11-02 11:30:21 +0300

    [Enhancement] Added find match wait time config variable Amount of time in seconds a user needs to wait before being able to find other matches - saves on db resources - Defaults to 30 seconds

--- src/config/data.config.js
+++ src/config/data.config.js
@@ -2,4 +2,7 @@
 module.exports.MAX_PROSPECTS = 100; 
 
 //? Max number of people you can match with in one instance
-module.exports.MAX_MATCHES = 5; 
+module.exports.MAX_MATCHES = 5;
+
+//? Amount of time in seconds a user needs to wait before being able to find other matches - saves on db resources
+module.exports.FIND_MATCH_WAIT_TIME = 30;
