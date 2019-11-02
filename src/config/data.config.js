c09b5ee59e89ec06078d08a7531c0efbbfbe0acf
AllanJeremy <aj.dev254@gmail.com>
2019-11-02 11:09:44 +0300

    Added the maximum number of searches for find matches request - We'll be returning the top `n` matches found in any matching request

--- src/config/data.config.js
+++ src/config/data.config.js
@@ -1,1 +1,5 @@
-module.exports.MAX_PROSPECTS = 10000;
+//? Max number of people to be considered in one search
+module.exports.MAX_PROSPECTS = 100; 
+
+//? Max number of people you can match with in one instance
+module.exports.MAX_MATCHES = 5; 
