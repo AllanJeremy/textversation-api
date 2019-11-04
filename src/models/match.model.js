636a5a3b530bfc617e1cbc5e0ec0530e90d19757
AllanJeremy <aj.dev254@gmail.com>
2019-11-04 17:31:26 +0300

    [Feature] Find matches working - Add matches & previously matched users working - Get previously matched users also works
    - TODO: Log the prospect data for analysis

--- src/models/match.model.js
+++ src/models/match.model.js
@@ -1,11 +1,15 @@
-const {db,firestore,admin} = require("../database");
+const {
+    db,
+    firestore,
+    admin
+} = require("../database");
 const FieldValue = admin.firestore.FieldValue;
 
 //? Controls how we fetch data - eg. limits on how much data we can fetch
-const DataConfig = require("../config/data.config"); 
+const DataConfig = require("../config/data.config");
 
-class MatchModel{
-    constructor(){
+class MatchModel {
+    constructor() {
         this.matchPoolRef = db.collection("matchPool");
         this.matchedWithBeforeRef = db.collection("matchedUsers");
         this.matchesRef = db.collection("matches");
@@ -14,26 +18,26 @@ class MatchModel{
 
     //* MATCH POOL
     // Returns a reference of a user in the match pool with the specified id
-    _userInMatchPoolRef(userId){
-        return this.matchPoolRef.where("id","==",userId);
+    _userInMatchPoolRef(userId) { //* Tested & Working
+        return this.matchPoolRef.where("id", "==", userId);
     }
 
     // Returns the user with the id of `userId` in the match pool
-    async userInPool(userId){ //* Tested & Working
-        let userQuerySnapshot = await this._userInMatchPoolRef(userId).get(); 
+    async userInPool(userId) { //* Tested & Working
+        let userQuerySnapshot = await this._userInMatchPoolRef(userId).get();
         return userQuerySnapshot.size > 0;
     }
 
     // Add to pool
-    async addToMatchPool(user){ //* Tested & Working
+    async addToMatchPool(user) { //* Tested & Working
         let userInPool = await this.userInPool(user.id);
 
         // Set the date joined if they are not already in the pool
-        if(!userInPool){
+        if (!userInPool) {
             user.dateJoined = FieldValue.serverTimestamp();
-            
-            return this.matchPoolRef.doc().set(user,{
-                merge:true
+
+            return this.matchPoolRef.doc().set(user, {
+                merge: true
             });
         }
 
@@ -41,47 +45,85 @@ class MatchModel{
         return false;
     }
 
-    removeFromMatchPool(userId){ //? DONE
+    removeFromMatchPool(userId) { //* Tested & Working
         return this._userInMatchPoolRef(userId).delete();
     }
 
     // Get prospects ~ people that are in match pool & match basic criteria
-    async getProspects(currentUser){//? DONE
+    async getProspects(currentUser) { //* Tested & Working
         /* Firebase note:
         //* You can only perform range comparisons (<, <=, >, >=) on a single field, and you can include at most one array-contains clause in a compound query
         https://firebase.google.com/docs/firestore/query-data/queries#compound_queries
         */
         return this.matchPoolRef
-            .where("gender","==",currentUser.preferences.gender) //? Add composite index for `gender`
-            .where("age",">=",currentUser.preferences.minAge) //? Add composite index for `age`
-            .where("age","<=",currentUser.preferences.maxAge) 
-            .orderBy("dateJoined","asc")//? First come first serve implementation
+            .where("gender", "==", currentUser.preferences.gender) //? Add composite index for `gender`
+            .where("age", ">=", currentUser.preferences.minAge) //? Add composite index for `age`
+            .where("age", "<=", currentUser.preferences.maxAge)
+            // .orderBy("dateJoined","asc")//? First come first serve implementation
             .limit(DataConfig.MAX_PROSPECTS)
             .get();
     }
 
     //* MATCH & MATCH LOGS
+    // Returns match data with the appropriate data attributes
+    _getMatchData(match){ //* Tested & Working
+        // Remove unnecessary fluff that isn't relevant for these collections
+        delete match.initiatorUser.interests;
+        delete match.prospectUser.interests;
+
+        return {
+            initiatorUser: match.initiatorUser,
+            prospectUser: match.prospectUser,
+            dateMatched: FieldValue.serverTimestamp()
+        };
+    }
 
     // Store matched users in a special collection ~ we will be referencing this collection when trying to find previously matched users
-    async _logMatchedUsers(matchedUsers){ //TODO: Actually figure out how we will pass this user data in
-       //TODO: Add implementation
+    async _storeMatchedUsers(matchData) { //* Tested & Working
+        let batch = db.batch();
+
+        matchData.map((match) => {
+            let matchedData = {
+                userIds: [match.initiatorUser.id, match.prospectUser.id],
+                ...this._getMatchData(match)
+            };
+
+            let matchedDocRef = this.matchedWithBeforeRef.doc();
+            batch.set(matchedDocRef, matchedData);
+        });
+        
+        return batch.commit();
     }
 
     // Add match ~ store details of successful match
-    async addMatch(matchDetails){
-        //TODO: Add implementation
+    async addMatch(matchData) { //* Tested & Working
+        this._storeMatchedUsers(matchData);
+        
+        let batch = db.batch();
 
-        // this._logMatchedUsers();
+        matchData.map((match)=>{
+            let matchInsertData = {
+                ...this._getMatchData(match),
+                isNew: true
+            };
+
+            let matchesDocRef = this.matchesRef.doc();
+            batch.set(matchesDocRef,matchInsertData);
+        });
+
+        return batch.commit();
     }
 
     // Save match details to db - logging
-    async saveMatchDataToDb(matchData){
+    async saveMatchDataToDb(matchData) {
 
     }
 
     // Get all users that previously matched with the user with the id of `userId`
-    async getPreviouslyMatchedUsers(userId){
-
+    async getPreviouslyMatchedUsers(userId) { //* Tested & Working
+        return this.matchedWithBeforeRef
+            .where("userIds", "array-contains", userId)
+            .get();
     }
 }
 
--- src/modules/match.js
+++ src/modules/match.js
@@ -1,69 +1,157 @@
-const Config = require("../config/config");
+const {firestore} = require("../database");
+
+const DataConfig = require("../config/data.config");
+const MatchConfig = require("../config/match.config");
+const User = require("./user");
+const MatchModel = require("../models/match.model");
+const MatchAlgo = require("../algorithms/match.algorithm");
 
 class Match{
-   
+    // Returns a user with the id as part of the object returned
+    async _getUserWithId(userId){
+        let currentUserDoc = await User.getUser(userId);
+        let currentUser = currentUserDoc.exists ? currentUserDoc.data() : null;
 
-    async _findPotentialMatchesInCache(userId){
-        //TOOD: Add implementation
+        // Return false if the user with the provided id was not foundcurrentUser
+        if(!currentUser){ //? {} Used for clarity's sake when reading the code
+            return false;
+        }
+
+        // Add the current user's id to the user returned
+        currentUser.id = userId;
+
+        // Remove other user fluff irrelevant to the users of this function
+        delete currentUser.lastLogin;
+        delete currentUser.loggedIn;
+
+        return currentUser;
     }
 
-    // Criteria that must match is matching, returns true or false
-    async _mainCriteriaMatch(userId1,userId2){
-        //TODO: Add implementation
+    // Get users that have previously matched with the user with the id of `userId`
+    async _getPreviouslyMatchedUserIds(currentUserId){
+        let prevMatchedUsersSnapshot = await MatchModel.getPreviouslyMatchedUsers(currentUserId); //! [read] Query 3
+
+        let prevMatchedUserIds = prevMatchedUsersSnapshot.docs.map((doc)=>{
+            let match = doc.data();
+            let currIdIndex = match.userIds.indexOf(currentUserId);
+            match.userIds.splice(currIdIndex,1);
+            return match.userIds[0]; //? Assumes match has 2 people ~ may need to change if we begin comparing more than 2 people
+            //TODO: Flatten array & return `match.userIds` to return all ids for `n` people matched ~ in future
+        });
+
+        return prevMatchedUserIds;
     }
 
-    // Store potential match details
-    async _storePotentialMatchDetails(userId1,userId2,matchDetails){
-        //TODO: Add implementation
+    // Removes previously matched users and returns users that match basic criteria
+    async _getProspects(currentUser){
+        let allProspects = await MatchModel.getProspects(currentUser);//! [read] Query 2 
+        if(allProspects.size == 0) return [];
 
+        //* Only get previously matched users if there are prospects ~ save on reads
+        let previouslyMatchedUserIds = await this._getPreviouslyMatchedUserIds(currentUser.id);
+
+        // Only return prospects that match basic criteria and we have not matched with before
+        let prospects = allProspects.docs.map((doc)=>{
+            let prospect = doc.data();
+            if(prospect.id == currentUser.id) return false;
+
+            let matchedBefore = previouslyMatchedUserIds.includes(prospect.id);
+            
+            // If we have matched with the prospect before ~ do not return them as a prospect
+            if(matchedBefore){
+                console.log(`Matched with this user (id: ${prospect.id}) before: Ignoring...`);
+                return false;
+            }
+
+            // Only return the prospect if we haven't matched with them before
+            return prospect;
+        });
+        prospects = prospects.filter(prospect=>prospect); //? Only return truthy values | AKA valid prospects in this case
+
+        return prospects;
     }
 
     // Interests match ~ returns a number between 0 and 1 representing how similar they are
-    async _getInterestMatchScore(userId1,userId2)
+    _getInterestMatchData(user1,user2)
     {
-        //TODO: Add implementation
+        let interestMatchData = MatchAlgo.ajInterestMatchCalc(user1.interests,user2.interests);
+        return interestMatchData;
     }
 
     //* HELPERS
     _getMatchVerdict(usersMatch){
         return usersMatch ? 'Match' : 'FailedMatch';
     }
-
-
+    
     //* -----
 
+    _limitMatchedUsers(matchedUsers){
+        return matchedUsers.filter((user,index)=>{
+            return index < DataConfig.MAX_MATCHES; //? Returns up to `MAX_MATCHES` matches
+        });
+    }
+
     // Returns true if the users are a good match ~ false if not
-    async _usersMatch(userId1,userId2){
-
-        const mainCriteriaMatch = await this._mainCriteriaMatch(userId1,userId2);
-
-        // Check if the match threshold
-        let interestMatchScore = this._getInterestMatchScore(userId,userId2);
-        
-        let usersMatch = mainCriteriaMatch && (interestMatchScore >= Config.MIN_INTEREST_MATCH_THRESHOLD);
+    async _storePotentialMatchDetails(user1,user2,interestMatchScore){//TODO: Needs proper implementation
+        let usersMatch = interestMatchScore >= MatchConfig.MIN_INTEREST_MATCH_THRESHOLD;
 
         let matchDetails = {
-            mainCriteriaMatched: usersMatch,
             interestMatchScore: interestMatchScore,
+            user1: user1,
+            user2: user2,
+            isMatch: usersMatch,
             verdict: _getMatchVerdict(usersMatch),
-            matchThreshold: Config.MIN_INTEREST_MATCH_THRESHOLD,
-            date: Date()//TODO: Add actual date to firebase
+            matchThreshold: MatchConfig.MIN_INTEREST_MATCH_THRESHOLD,
+            date: FieldValue.serverTimestamp()
         };
 
-
-        //? We may want to start logging this
-        this._storePotentialMatchDetails(userId,userId2,matchDetails);
+        // TODO: Store potential match details
 
         return usersMatch;
     }
-  
+
     //* PUBLIC METHODS
     // Find and return matches for given user
-    async findMatches(userId){
-        let user = {};
+    async findMatches(userId){ 
+        //TODO: Check if the user has waited long enough before trying to perform another request
+        let currentUser = await this._getUserWithId(userId); //! [read] Query 1 
 
-        //TODO: Add implementation
+        // If the current user was not found ~ don't bother trying to find matches
+        if(!currentUser) return [];
+
+        const prospects = await this._getProspects(currentUser);
+        //* PROSPECTS WITH INTEREST MATCHING SCORE CALCULATED
+        // Prospects whose interest matching values have been calculated
+        let computedProspects = prospects.map((prospect)=>{
+            let matchData = this._getInterestMatchData(currentUser,prospect);
+            
+            let prospectData = {
+                initiatorUser: currentUser,
+                prospectUser: prospect,
+                ...matchData
+            };
+            return prospectData;
+        });
+        //TODO: Log the computed prospects
+
+
+        //* MATCHED USERS
+        // Prospects that were actually a match
+        let matchedProspects = computedProspects.filter((computedProspect)=>{
+            return computedProspect.isMatch;
+        });
+        
+        // Top `MAX_MATCHES` matches returned only ~ Add to previously matched users
+        let finalMatchedProspects = this._limitMatchedUsers(matchedProspects);
+       
+        // Add match : also adds to previously matched users
+        MatchModel.addMatch(finalMatchedProspects); //? Working
+
+        //* MATCH POOL
+        // Add the current user to the matching pool so that other users can match with him/her
+        MatchModel.addToMatchPool(currentUser); //! [write] Query 4
+        return finalMatchedProspects;
     }
 }
 
-module.exports = Match;
+module.exports = new Match();
