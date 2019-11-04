682ab980826efdd3a857de0a31bb006ed452558e
AllanJeremy <aj.dev254@gmail.com>
2019-11-04 17:28:22 +0300

    Added find matches endpoint

--- src/routes/match.route.js
+++ src/routes/match.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const MatchMiddleware = require("../middleware/match.middleware");
 
-//
+// Find matches for user with the id of `uid`
+router.post("/find/:uid",MatchMiddleware.findMatches);
 
 module.exports = router;
