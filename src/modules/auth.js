6b65ef21a8a3273250bd534e9f4785c716ab538c
AllanJeremy <aj.dev254@gmail.com>
2019-11-13 12:17:58 +0300

    Generating tokens for when a user logs in - Later, we will be using this token to perform operations that require user authentication (at least preventing mistakes from a client side)

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,33 +1,42 @@
-const UserModel = require("../models/user.model");
-const {admin} = require("../database");
-const FieldValue = admin.firestore.FieldValue;
+const cryptoRandomString = require('crypto-random-string');
 
-class Auth{
+const UserModel = require("../models/user.model");
+const {
+    admin
+} = require("../database");
+const FieldValue = admin.firestore.FieldValue;
+class Auth {
     //* HELPERS
-    async _updateLoginStatus(uid,isLoggedIn){
+    async _updateLoginStatus(uid, isLoggedIn) {
         let updateData = {
             loggedIn: isLoggedIn,
         };
 
         // Only save last login if we are logging in
-        if(isLoggedIn){
-            updateData.lastLogin = FieldValue.serverTimestamp();
+        if (isLoggedIn) {
+            // Generate a random user token
+            let token = cryptoRandomString({
+                length: 16,
+                type: 'base64'
+            });
+            updateData.token = token;
+            updateData.lastLogin = FieldValue.serverTimestamp(); //TODO: Refactor this into model somehow
         }
 
-        let updateUserStatus = UserModel.updateUser(uid,updateData);
+        let updateUserStatus = UserModel.updateUser(uid, updateData);
         return updateUserStatus;
     }
 
     //* -------
 
     // Login ~ there is no signup since we are using anonymous logins
-    async login(uid){
-        return this._updateLoginStatus(uid,true);
+    async login(uid) {
+        return this._updateLoginStatus(uid, true);
     }
 
     // Update the status of the user to logged out
-    async logout(uid){
-        return this._updateLoginStatus(uid,false);
+    async logout(uid) {
+        return this._updateLoginStatus(uid, false);
     }
 }
 
