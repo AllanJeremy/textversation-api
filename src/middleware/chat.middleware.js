7cfbb8743c9cee39579c21d4e7a91bf5a11f7d10
AllanJeremy <aj.dev254@gmail.com>
2019-11-13 12:18:31 +0300

    Added chat middleware

---
+++ src/middleware/chat.middleware.js
@@ -0,0 +1,17 @@
+const Api = require("../lib/api");
+const Chat = require("../modules/Chat");
+
+module.exports.sendMessage = (req,res,next)=>{
+    Api.attachErrorHandler(res,
+        Chat.sendMessage(req.params.chatId,req.body.data).then((response)=>{
+            const data = {
+                message: req.body.data
+            };
+            const responseStatus = 201;
+            let responseData = Api.getResponse(true,"Successfully sent message",data,responseStatus);
+
+            res.status(responseData.statusCode).json(responseData);
+            next();
+        })
+    );
+};
