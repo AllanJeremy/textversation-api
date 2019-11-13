55221d53345aa8bf302df3da30e37dc2ebcd126d
AllanJeremy <aj.dev254@gmail.com>
2019-11-13 12:18:23 +0300

    [Feature] Added create new chat & send message functionality

---
+++ src/models/chat.model.js
@@ -0,0 +1,50 @@
+const {
+    db,
+    admin
+} = require("../database");
+const FieldValue = admin.firestore.FieldValue;
+
+class ChatModel {
+    constructor() {
+        this.chatCollectionRef = db.collection("chatThreads");
+    }
+
+    // Filters out unnecessary parts of the user data
+    _getChatData(match) {
+        let chatData = {
+            participants: [match.initiatorUser.id,match.prospectUser.id],
+            messages: [],
+            dateStarted: FieldValue.serverTimestamp()
+        };
+
+        return chatData;
+    }
+
+    // Used implicitly when we match with someone
+    newChats(matchData) { //? Batch
+        // Messages
+        let batch = db.batch();
+        // Users
+        matchData.map((match) => {
+            let chatData = this._getChatData(match);
+
+            let chatDocRef = this.chatCollectionRef.doc();
+            batch.set(chatDocRef,chatData);
+        });
+        
+        return batch.commit();
+    }
+
+    // Send a message
+    async sendMessage(chatId, messageData) { //! Delete message won't work with this structure, no message id generated/provided
+        let insertData = {
+            messages: [messageData]
+        };
+
+        return this.chatCollectionRef.doc(chatId).set(insertData, {
+            merge: true
+        });
+    }
+}
+
+module.exports = ChatModel();
---
+++ src/modules/chat.js
@@ -0,0 +1,30 @@
+const ChatModel = require("../models/chat.model");
+
+class Chat {
+    constructor() {
+        console.log("New chat created");
+    }
+    _cleanseMessageData(messageData) {
+        let cleansedData = {
+            from: messageData.from,
+            to: messageData.to,
+            text: messageData.text,
+            media: messageData.media,
+        };
+
+        return cleansedData;
+    }
+
+    sendMessage(chatId, messageData) {
+        messageData = this._cleanseMessageData(messageData);
+
+        return ChatModel.sendMessage(chatId, messageData);
+    }
+
+    newChats(matchData){
+        return ChatModel.newChats(matchData);
+    }
+}
+
+
+module.exports = new Chat();
