32c9874e0bfd2e876253f9b5ae7f2714946e4592
AllanJeremy <aj.dev254@gmail.com>
2019-11-16 11:53:39 +0300

    [Major enhancement] Chat thread & chats are now decoupled - A chat thread contains the last message from a chat. This reduces the number of messages we have to request when fetching a single user's chat threads thus reducing data consumption
    - TODO: Consider storing the number of unread messages in a chat. (Possible expense)

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -6,34 +6,44 @@ const FieldValue = admin.firestore.FieldValue;
 
 class ChatModel {
     constructor() {
-        this.chatCollectionRef = db.collection("chatThreads");
+        this.chatThreadCollectionRef = db.collection("chatThreads");
+        this.chatCollectionRef = db.collection("chats");
     }
 
     // Filters out unnecessary parts of the user data
-    _getChatData(match) {
-        let chatData = {
+    _getChatThreadData(match) {
+        let chatThreadData = {
             participantIds: [match.initiatorUser.id,match.prospectUser.id],
             userData: {
                 initiator: match.initiatorUser,
                 prospect: match.prospectUser
             },
             interestsMatched: match.interestsMatched,
-            messages: [],
             dateStarted: FieldValue.serverTimestamp()
+        };
+
+        return chatThreadData;
+    }
+
+    _getChatData(chatThreadId,messageData){
+        let chatData = {
+            userIds: [messageData.from.id,messageData.to.id], //! Possible vulnerability - if user passes random ids, we may not be able to prevent them from sending a message to other users
+            ...messageData,
+            threadId: chatThreadId,
+            dateSent: FieldValue.serverTimestamp()
         };
 
         return chatData;
     }
-
     // Used implicitly when we match with someone
     newChats(matchData) { //? Batch
         // Messages
         let batch = db.batch();
         // Users
         matchData.map((match) => {
-            let chatData = this._getChatData(match);
+            let chatData = this._getChatThreadData(match);
 
-            let chatDocRef = this.chatCollectionRef.doc();
+            let chatDocRef = this.chatThreadCollectionRef.doc();
             batch.set(chatDocRef,chatData);
         });
         
@@ -41,14 +51,20 @@ class ChatModel {
     }
 
     // Send a message
-    async sendMessage(chatId, messageData) { //! Delete message won't work with this structure, no message id generated/provided
-        let insertData = {
-            messages: [messageData]
+    async sendMessage(chatThreadId, messageData) { //! Delete message won't work with this structure, no message id generated/provided
+        let threadInsertData = {
+            lastMessage: messageData,
         };
 
-        return this.chatCollectionRef.doc(chatId).set(insertData, {
+        // Add the last message to the chat thread ~ we will be referencing this when displaying messages
+        this.chatThreadCollectionRef.doc(chatThreadId).set(threadInsertData, {
             merge: true
         });
+
+        let chatInsertData = this._getChatData(chatThreadId,messageData);
+
+        // Add the actual chat to a chat file
+        return this.chatCollectionRef.doc().set(chatInsertData,{merge: true});
     }
 }
 
