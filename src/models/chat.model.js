dff31afa5fac2c884cd278625bf0999d0ec3debd
AllanJeremy <aj.dev254@gmail.com>
2019-11-20 18:20:47 +0300

    [Enhancement] Passing full chat data when saving last message in a thread - This allows us be sure that anytime we access `lastMessage` we have access to the same exact data that the chats collection contains

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -1,71 +1,70 @@
-const {
-    db,
-    admin
-} = require("../database");
+const { db, admin } = require("../database");
 const FieldValue = admin.firestore.FieldValue;
 
 class ChatModel {
-    constructor() {
-        this.chatThreadCollectionRef = db.collection("chatThreads");
-        this.chatCollectionRef = db.collection("chats");
-    }
+  constructor() {
+    this.chatThreadCollectionRef = db.collection("chatThreads");
+    this.chatCollectionRef = db.collection("chats");
+  }
 
-    // Filters out unnecessary parts of the user data
-    _getChatThreadData(match) {
-        let chatThreadData = {
-            participantIds: [match.initiatorUser.id,match.prospectUser.id],
-            userData: {
-                initiator: match.initiatorUser,
-                prospect: match.prospectUser
-            },
-            interestsMatched: match.interestsMatched,
-            dateStarted: FieldValue.serverTimestamp()
-        };
+  // Filters out unnecessary parts of the user data
+  _getChatThreadData(match) {
+    let chatThreadData = {
+      participantIds: [match.initiatorUser.id, match.prospectUser.id],
+      userData: {
+        initiator: match.initiatorUser,
+        prospect: match.prospectUser
+      },
+      interestsMatched: match.interestsMatched,
+      dateStarted: FieldValue.serverTimestamp()
+    };
 
-        return chatThreadData;
-    }
+    return chatThreadData;
+  }
 
-    _getChatData(chatThreadId,messageData){
-        let chatData = {
-            userIds: [messageData.from.id,messageData.to.id], //! Possible vulnerability - if user passes random ids, we may not be able to prevent them from sending a message to other users
-            ...messageData,
-            threadId: chatThreadId,
-            dateSent: FieldValue.serverTimestamp()
-        };
+  _getChatData(chatThreadId, messageData) {
+    let chatData = {
+      userIds: [messageData.from.id, messageData.to.id], //! Possible vulnerability - if user passes random ids, we may not be able to prevent them from sending a message to other users
+      ...messageData,
+      threadId: chatThreadId,
+      dateSent: FieldValue.serverTimestamp()
+    };
 
-        return chatData;
-    }
-    // Used implicitly when we match with someone
-    newChats(matchData) { //? Batch
-        // Messages
-        let batch = db.batch();
-        // Users
-        matchData.map((match) => {
-            let chatData = this._getChatThreadData(match);
+    return chatData;
+  }
+  // Used implicitly when we match with someone
+  newChats(matchData) {
+    //? Batch
+    // Messages
+    let batch = db.batch();
+    // Users
+    matchData.map(match => {
+      let chatData = this._getChatThreadData(match);
 
-            let chatDocRef = this.chatThreadCollectionRef.doc();
-            batch.set(chatDocRef,chatData);
-        });
-        
-        return batch.commit();
-    }
+      let chatDocRef = this.chatThreadCollectionRef.doc();
+      batch.set(chatDocRef, chatData);
+    });
 
-    // Send a message
-    async sendMessage(chatThreadId, messageData) { //! Delete message won't work with this structure, no message id generated/provided
-        let threadInsertData = {
-            lastMessage: messageData,
-        };
+    return batch.commit();
+  }
 
-        // Add the last message to the chat thread ~ we will be referencing this when displaying messages
-        this.chatThreadCollectionRef.doc(chatThreadId).set(threadInsertData, {
-            merge: true
-        });
+  // Send a message
+  async sendMessage(chatThreadId, messageData) {
+    //! Delete message won't work with this structure, no message id generated/provided
+    let chatInsertData = this._getChatData(chatThreadId, messageData);
 
-        let chatInsertData = this._getChatData(chatThreadId,messageData);
+    let threadInsertData = {
+      lastMessage: chatInsertData
+    };
 
-        // Add the actual chat to a chat file
-        return this.chatCollectionRef.doc().set(chatInsertData,{merge: true});
-    }
+    // Add the last message to the chat thread ~ we will be referencing this when displaying messages
+    this.chatThreadCollectionRef.doc(chatThreadId).set(threadInsertData, {
+      merge: true
+    });
+
+    // Add the actual chat to a chat file
+    return this.chatCollectionRef.doc().set(chatInsertData, { merge: true });
+  }
 }
 
 module.exports = new ChatModel();
