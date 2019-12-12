9fc1cd67719b31f8a8e0adff327e3b80b21ffd7e
AllanJeremy <aj.dev254@gmail.com>
2019-12-12 19:32:21 +0300

    [Feature] Update all user nicknames working

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -2,88 +2,121 @@ const User = require("../modules/user");
 const Api = require("../lib/api");
 
 // Simply refactoring to keep code DRY
-function _setUpdateResponse(req,res,next,updatedItemName){
-    let statusCode = 201;
-    let responseData = Api.getResponse(true,`Successfully updated ${updatedItemName}`,{
-        newValue: req.body.newValue 
-    },statusCode);
+function _setUpdateResponse(req, res, next, updatedItemName) {
+  let statusCode = 201;
+  let responseData = Api.getResponse(
+    true,
+    `Successfully updated ${updatedItemName}`,
+    {
+      newValue: req.body.newValue
+    },
+    statusCode
+  );
 
-    res.status(201).json(responseData);
-    next();
+  res.status(201).json(responseData);
+  next();
 }
 
 // Update user
-module.exports.updateUser = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.updateUser(req.body.uid,req.body.data)
-        .then((response)=>{
-            _setUpdateResponse(req,res,next,'user');
-        })
-    );
+module.exports.updateUser = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.updateUser(req.body.uid, req.body.data).then(response => {
+      _setUpdateResponse(req, res, next, "user");
+    })
+  );
 };
 
 // Set age
-module.exports.setAge = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.setAge(req.body.uid,req.body.newValue)
-        .then((response)=>{
-            _setUpdateResponse(req,res,next,'age');
-        })
-    );
+module.exports.setAge = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.setAge(req.body.uid, req.body.newValue).then(response => {
+      _setUpdateResponse(req, res, next, "age");
+    })
+  );
 };
 
 // Set the gender
-module.exports.setGender = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.setGender(req.body.uid,req.body.newValue)
-        .then((response)=>{
-            _setUpdateResponse(req,res,next,'gender');
-        }),
-    );
+module.exports.setGender = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.setGender(req.body.uid, req.body.newValue).then(response => {
+      _setUpdateResponse(req, res, next, "gender");
+    })
+  );
 };
 
 // Set age preference
-module.exports.setAgePreference = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.setAgePreference(req.body.uid,req.body.newValue)
-        .then((response)=>{
-            _setUpdateResponse(req,res,next,'age preference');
-        })
-    );
+module.exports.setAgePreference = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.setAgePreference(req.body.uid, req.body.newValue).then(response => {
+      _setUpdateResponse(req, res, next, "age preference");
+    })
+  );
 };
 
 // Set gender preference
-module.exports.setGenderPreference = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.setGenderPreference(req.body.uid,req.body.newValue)
-        .then((response)=>{
-            _setUpdateResponse(req,res,next,'gender preference');
-        })
-    );
+module.exports.setGenderPreference = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.setGenderPreference(req.body.uid, req.body.newValue).then(response => {
+      _setUpdateResponse(req, res, next, "gender preference");
+    })
+  );
 };
 
 // Add interests
-module.exports.addInterests = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.setInterests(req.body.uid,req.body.interests)
-        .then((response)=>{
-            let responseData = Api.getResponse(true,"Successfully added interests",req.body.interests,201);
+module.exports.addInterests = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.setInterests(req.body.uid, req.body.interests).then(response => {
+      let responseData = Api.getResponse(
+        true,
+        "Successfully added interests",
+        req.body.interests,
+        201
+      );
 
-            res.status(responseData.statusCode).json(responseData);
-            next();
-        })
-    );
+      res.status(responseData.statusCode).json(responseData);
+      next();
+    })
+  );
 };
 
 // Remove an interest
-module.exports.removeInterest = (req,res,next)=>{
-    Api.attachErrorHandler(res,
-        User.removeInterest(req.body.uid,req.params.interestId)
-        .then((response)=>{
-            let responseData = Api.getResponse(true,"Successfully removed interest",response,201);
+module.exports.removeInterest = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.removeInterest(req.body.uid, req.params.interestId).then(response => {
+      let responseData = Api.getResponse(
+        true,
+        "Successfully removed interest",
+        response,
+        201
+      );
 
-            res.status(responseData.statusCode).json(responseData);
-            next();
-        })
-    );
+      res.status(responseData.statusCode).json(responseData);
+      next();
+    })
+  );
+};
+
+// Update all user nicknames
+//? Possibly needs optimization once we get more than 1000 users
+module.exports.updateAllUserNicknames = (req, res, next) => {
+  Api.attachErrorHandler(
+    res,
+    User.updateNicknames(req.body.data).then(() => {
+      let responseData = Api.getResponse(
+        true,
+        "Successfully updated user nicknames",
+        null,
+        201
+      );
+      res.status(responseData.statusCode).json(responseData);
+      next();
+    })
+  );
 };
--- src/modules/user.js
+++ src/modules/user.js
@@ -1,116 +1,127 @@
 const UserModel = require("../models/user.model");
 const chance = require("chance").Chance();
 
-class User{
-    getUserNickname(gender,maxLength){
-        maxLength = maxLength || 15;
-    
-        let nickname = chance.name({
-            "gender": gender
-        });
-    
-        if(nickname.length > maxLength){
-            _getUserNickname(gender,maxLength);
-        }else{
-            return nickname;
-        }
+class User {
+  getUserNickname(gender, maxLength) {
+    maxLength = maxLength || 15;
+
+    let nickname = chance.name({
+      gender: gender
+    });
+
+    if (nickname.length > maxLength) {
+      _getUserNickname(gender, maxLength);
+    } else {
+      return nickname;
+    }
+  }
+
+  //* ACTUAL DETAIL IMPLEMENTATION
+  async getUser(userId) {
+    let user = await UserModel.getUser(userId);
+    return user;
+  }
+
+  async updateUser(userId, updateData) {
+    // If there is no user data, don't bother
+    if (!userId || !updateData) return false;
+
+    // Generate a new nickname if we gender is provided as an update value
+    if (updateData.gender) {
+      updateData.nickname = this.getUserNickname(updateData.gender);
     }
 
-    //* ACTUAL DETAIL IMPLEMENTATION
-    async getUser(userId){
-        let user = await UserModel.getUser(userId);
-        return user;
-    }
+    return UserModel.updateUser(userId, updateData);
+  }
 
-    async updateUser(userId,updateData){
-        // If there is no user data, don't bother
-        if(!userId || !updateData) return false;
+  //* ABSTRACTIONS
+  async setAge(userId, newAge) {
+    let updateData = {
+      age: newAge
+    };
 
-        // Generate a new nickname if we are updating the gender
-        if(updateData.gender){
-            updateData.nickname = this.getUserNickname(updateData.gender);
-        }
+    return this.updateUser(userId, updateData);
+  }
 
-        return UserModel.updateUser(userId,updateData);
-    }
+  async setGender(userId, newGender) {
+    let updateData = {
+      gender: newGender
+    };
 
-    //* ABSTRACTIONS
-    async setAge(userId,newAge){
-        let updateData = {
-            age: newAge
-        };
+    updateData.nickname = this.getUserNickname(newGender);
 
-        return this.updateUser(userId,updateData);
-    }
+    return this.updateUser(userId, updateData);
+  }
 
-    async setGender(userId,newGender){
-        let updateData = {
-            gender: newGender
-        };
+  async setGenderPreference(userId, newGenderPreference) {
+    let updateData = {
+      preferences: {
+        gender: newGenderPreference
+      }
+    };
 
-        updateData.nickname = this.getUserNickname(newGender);
+    return this.updateUser(userId, updateData);
+  }
 
-        return this.updateUser(userId,updateData);
-    }
+  async setAgePreference(userId, ageRange) {
+    // At least one must have been set to proceeed with update
+    if (!ageRange.min && !ageRange.max) return false;
 
-    async setGenderPreference(userId,newGenderPreference){
-        let updateData = {
-            preferences: {
-                gender: newGenderPreference
-            }
-        };
+    let updateData = {
+      preferences: {
+        minAge: ageRange.min,
+        maxAge: ageRange.max
+      }
+    };
 
-        return this.updateUser(userId,updateData);
-    }
+    return this.updateUser(userId, updateData);
+  }
 
-    async setAgePreference(userId,ageRange){
-        // At least one must have been set to proceeed with update
-        if((!ageRange.min && !ageRange.max)) return false;
+  // Get user interests
+  async getUserInterests(userId) {
+    let user = await this.getUser(userId);
+    let userData = user.data();
 
-        let updateData = {
-            preferences: {
-                minAge: ageRange.min,
-                maxAge: ageRange.max
-            }
-        };
+    return user.exists ? userData.interests : {};
+  }
 
-        return this.updateUser(userId,updateData);
-    }
+  //* HELPER FUNCTIONS
+  // Convert interests array to object
+  _getInterestsObject(interests) {
+    //? [Reason] Makes it easier for us to delete interests when they are objects in firebase
+    let interestObject = {};
+    interests.map(interest => {
+      interestObject[interest.id] = interest;
+    });
 
-    // Get user interests
-    async getUserInterests(userId){
-        let user = await this.getUser(userId);
-        let userData = user.data();
+    return interestObject;
+  }
 
-        return user.exists ? userData.interests : {};
-    }
+  // Adds an object containing interests
+  async setInterests(userId, interests) {
+    let updateData = {
+      interests: this._getInterestsObject(interests)
+    };
 
-    //* HELPER FUNCTIONS
-    // Convert interests array to object
-    _getInterestsObject(interests){ //? [Reason] Makes it easier for us to delete interests when they are objects in firebase
-        let interestObject = {};
-        interests.map((interest)=>{
-            interestObject[interest.id] = interest;
-        });
+    // updateData[interest.key] = { interestName: , interestType: , interestDescription: ,}
 
-        return interestObject;
-    }
+    return this.updateUser(userId, updateData);
+  }
 
-    // Adds an object containing interests
-    async setInterests(userId,interests){
-        let updateData = {
-            interests: this._getInterestsObject(interests)
-        };
+  //
+  async removeInterest(userId, interestId) {
+    //? Depends on user model directly
+    return UserModel.removeInterest(userId, interestId);
+  }
 
-        // updateData[interest.key] = { interestName: , interestType: , interestDescription: ,}
-
-        return this.updateUser(userId,updateData);
-    }
-
-    // 
-    async removeInterest(userId,interestId){ //? Depends on user model directly
-        return UserModel.removeInterest(userId,interestId);
-    }
+  //! n writes - where n = number of users
+  async updateNicknames(users) {
+    users.forEach(user => {
+      let updateData = { gender: user.gender };
+      this.updateUser(user.id, updateData);
+    });
+    return true;
+  }
 }
 
 module.exports = new User();
